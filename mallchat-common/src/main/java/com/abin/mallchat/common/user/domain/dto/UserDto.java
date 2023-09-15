cc1f0a36de720a087f238325debd81efdb3f4025
wangzi02 <wangzi02@longfor.com>
2023-09-15 15:56:41 +0800

    添加userdto：name

---
+++ mallchat-common/src/main/java/com/abin/mallchat/common/user/domain/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.abin.mallchat.common.user.domain.dto;
+
+import lombok.Data;
+
+/**
+ * @author wangzi02
+ * @description
+ * @create 2023/9/15 15:55
+ */
+@Data
+public class UserDto {
+    private String name;
+}
